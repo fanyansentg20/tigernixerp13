6099fa1fb83f90a90af8b43a1ee8fef6b719199c
Fan Yansen <fan.yansen@tigernix.com>
2026-09-16 13:12:00 +0700

    Duplication of Payment Stripe Module

---
+++ tigernix/addons/payment_hitpay/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Part of TigernixERP. See LICENSE file for full copyright and licensing details.
+
+from . import models
+from . import controllers
+from tigernix.addons.payment.models.payment_acquirer import create_missing_journal_for_acquirers
+
